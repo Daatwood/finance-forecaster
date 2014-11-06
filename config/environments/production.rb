a7d66e9be8ca39de913aed6a11365e2a423b73b3
Dustin <daa@adfitech.com>
2014-11-05 19:10:40 -0600

    added host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method   = :postmark
   config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.default_url_options = { :host => 'finance.ryknzu.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
