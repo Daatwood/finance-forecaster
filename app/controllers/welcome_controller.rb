b9c4ccca4beed17e34146e5d4dfe2c688114dfc9
Dustin Atwood <dustin.atwood@adfitech.com>
2015-04-06 10:35:10 -0500

    redirect to dashboard instead of root

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    if user_signed_in?
+      redirect_to dashboard_path
+      return
+    end
   end
 
   # GET Let user resend invitation
