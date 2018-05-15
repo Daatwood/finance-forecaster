e4bba3dfdd31f8148d463fd35bb7a9d5fe969799
Dustin Atwood <dustin@atwood.email>
2018-05-14 19:06:52 -0500

    Fixed typo

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
   def index
     @bills = current_user.bills
     @transaction = current_user.bank.transactions.new
-    unless @bill.blank?
+    unless @bills.blank?
       @forecast = ForecastTransactions.call(current_user)
       @chart_data = CalculateBalanceChart.call(@forecast)
     end
